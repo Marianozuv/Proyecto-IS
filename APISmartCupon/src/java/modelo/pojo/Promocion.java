12034074294e9b4bcf7e740ba461bcd500f1d5ec
Mattai16 <111460191+Mattai16@users.noreply.github.com>
2023-11-26 21:12:05 -0600

    :white_check_mark: Pojos
    Modelado de las tablas de las bases de datos a ocupar

---
+++ APISmartCupon/src/java/modelo/pojo/Promocion.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo.pojo;
+
+/**
+ *
+ * @author mateo
+ */
+public class Promocion {
+
+    private Integer idPromocion;
+    private Integer idEmpresa;
+    private String nombrePromocion;
+    private String descripcion;
+    private String imagenPromocionBase64;
+    private String fechaInicioPromocion;
+    private String fechaTerminoPromocion;
+    private String restricciones;
+    private Integer idTipoPromocion;
+    private Integer porcentaje_Costo;
+    private Integer idCategoria;
+    private int cuponesMaximos;
+    private Integer codigoPromocion;
+    private boolean estatus;
+
+}
---
+++ APISmartCupon/src/java/modelo/pojo/Sucursal.java
@@ -0,0 +1,123 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo.pojo;
+
+/**
+ *
+ * @author mateo
+ */
+public class Sucursal {
+    
+    private Integer idSucursal;
+    private Integer idEmpresa;
+    private String nombreSucursal;
+    private String direccion;
+    private String codigoPostal;
+    private String colonia;
+    private String ciudad;
+    private String telefono;
+    private String latitud;
+    private String nombreEncargado;
+
+    public Sucursal() {
+    }
+
+    public Sucursal(Integer idSucursal, Integer idEmpresa, String nombreSucursal, String direccion, String codigoPostal, String colonia, String ciudad, String telefono, String latitud, String nombreEncargado) {
+        this.idSucursal = idSucursal;
+        this.idEmpresa = idEmpresa;
+        this.nombreSucursal = nombreSucursal;
+        this.direccion = direccion;
+        this.codigoPostal = codigoPostal;
+        this.colonia = colonia;
+        this.ciudad = ciudad;
+        this.telefono = telefono;
+        this.latitud = latitud;
+        this.nombreEncargado = nombreEncargado;
+    }
+
+    public Integer getIdSucursal() {
+        return idSucursal;
+    }
+
+    public void setIdSucursal(Integer idSucursal) {
+        this.idSucursal = idSucursal;
+    }
+
+    public Integer getIdEmpresa() {
+        return idEmpresa;
+    }
+
+    public void setIdEmpresa(Integer idEmpresa) {
+        this.idEmpresa = idEmpresa;
+    }
+
+    public String getNombreSucursal() {
+        return nombreSucursal;
+    }
+
+    public void setNombreSucursal(String nombreSucursal) {
+        this.nombreSucursal = nombreSucursal;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCodigoPostal() {
+        return codigoPostal;
+    }
+
+    public void setCodigoPostal(String codigoPostal) {
+        this.codigoPostal = codigoPostal;
+    }
+
+    public String getColonia() {
+        return colonia;
+    }
+
+    public void setColonia(String colonia) {
+        this.colonia = colonia;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getLatitud() {
+        return latitud;
+    }
+
+    public void setLatitud(String latitud) {
+        this.latitud = latitud;
+    }
+
+    public String getNombreEncargado() {
+        return nombreEncargado;
+    }
+
+    public void setNombreEncargado(String nombreEncargado) {
+        this.nombreEncargado = nombreEncargado;
+    }
+    
+    
+}
+
